fb71a9d89d331144567be1e61663d410f92283f3
TheQueenIsDead <30706552+TheQueenIsDead@users.noreply.github.com>
2022-01-25 20:12:04 +1300

    Order subdomains for local alphabetically

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -1,13 +1,15 @@
 locals {
-  ingress_ip = "192.168.0.69"
-  local_subdomains    = {
+  ingress_ip       = "192.168.0.69"
+  # All subdomains for the `local` domain
+  # Eg, "demo.local"
+  local_subdomains = {
     "demo"      = local.ingress_ip
+    "home"      = local.ingress_ip
     "homer"     = local.ingress_ip
-    "home"     = local.ingress_ip
     "ingress"   = local.ingress_ip
     "pihole"    = local.ingress_ip
-    "speedtest" = local.ingress_ip
     "radarr"    = local.ingress_ip
     "sonarr"    = local.ingress_ip
+    "speedtest" = local.ingress_ip
   }
 }
