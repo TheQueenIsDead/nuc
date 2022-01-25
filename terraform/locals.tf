6dc7ea7f05cee5d2371a98952959d29cdd6440b8
TheQueenIsDead <30706552+TheQueenIsDead@users.noreply.github.com>
2022-01-25 21:03:43 +1300

    Add node-exporter, prometheus, and grafana

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -4,10 +4,12 @@ locals {
   # Eg, "demo.local"
   local_subdomains = {
     "demo"      = local.ingress_ip
+    "grafana"   = local.ingress_ip
     "home"      = local.ingress_ip
     "homer"     = local.ingress_ip
     "ingress"   = local.ingress_ip
     "pihole"    = local.ingress_ip
+    "prom"      = local.ingress_ip
     "radarr"    = local.ingress_ip
     "sonarr"    = local.ingress_ip
     "speedtest" = local.ingress_ip
