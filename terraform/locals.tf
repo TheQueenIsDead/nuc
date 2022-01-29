2eec8ac0e2154e048284e316cb81902dc47ff088
TheQueenIsDead <30706552+TheQueenIsDead@users.noreply.github.com>
2022-01-29 23:17:39 +1300

    Add prowlarr and transmission

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -3,14 +3,17 @@ locals {
   # All subdomains for the `local` domain
   # Eg, "demo.local"
   local_subdomains = {
-    "demo"      = local.ingress_ip
-    "grafana"   = local.ingress_ip
-    "home"      = local.ingress_ip
-    "ingress"   = local.ingress_ip
-    "pihole"    = local.ingress_ip
-    "prom"      = local.ingress_ip
-    "radarr"    = local.ingress_ip
-    "sonarr"    = local.ingress_ip
-    "speedtest" = local.ingress_ip
+    "demo"         = local.ingress_ip
+    "grafana"      = local.ingress_ip
+    "home"         = local.ingress_ip
+    "ingress"      = local.ingress_ip
+    "jackett"      = local.ingress_ip
+    "pihole"       = local.ingress_ip
+    "prom"         = local.ingress_ip
+    "prowlarr"     = local.ingress_ip
+    "radarr"       = local.ingress_ip
+    "sonarr"       = local.ingress_ip
+    "speedtest"    = local.ingress_ip
+    "transmission" = local.ingress_ip
   }
 }
