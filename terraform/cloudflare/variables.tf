77e074477821e1a06cc3b8c9a0048a53486b06d7
TheQueenIsDead <30706552+thequeenisdead@users.noreply.github.com>
2026-02-17 14:50:49 +1300

    feat: add homarr

--- terraform/cloudflare/variables.tf
+++ terraform/cloudflare/variables.tf
@@ -26,6 +26,7 @@ variable "subdomains" {
     { name = "actual" },
     { name = "budge" },
     { name = "chat", public = true },
+    { name = "home", public = false },
     { name = "jellyfin", public = true },
     { name = "jellyseer", public = true },
     { name = "memos" },
